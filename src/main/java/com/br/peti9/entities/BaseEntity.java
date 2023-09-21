fb96e269fb27add03a04d499fbbe5d9fac47bfea
Alisson Pereira <alissonpereiratm@gmail.com>
2023-09-21 14:12:05 -0300

    application.properties

--- src/main/java/com/br/peti9/entities/BaseEntity.java
+++ src/main/java/com/br/peti9/entities/BaseEntity.java
@@ -1,13 +1,10 @@
 package com.br.peti9.entities;
 
-import java.time.LocalDateTime;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.PrePersist;
 
 @MappedSuperclass
 public class BaseEntity {
