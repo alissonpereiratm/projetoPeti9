b75feb514a2234351d1a34e6abdb0131db135346
Alisson Pereira <alissonpereiratm@gmail.com>
2023-09-29 14:53:53 -0300

    teste Entity Tutor

--- src/main/java/com/br/peti9/controllers/PetController.java
+++ src/main/java/com/br/peti9/controllers/PetController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.br.peti9.entities.Pet;
 import com.br.peti9.services.PetService;
 
-
 @RestController
 @RequestMapping(value = "/pet")
 public class PetController {
--- src/main/java/com/br/peti9/controllers/TutorController.java
+++ src/main/java/com/br/peti9/controllers/TutorController.java
@@ -1,13 +1,9 @@
 package com.br.peti9.controllers;
 
-import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,12 +11,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.br.peti9.entities.Tutor;
 import com.br.peti9.services.TutorService;
-
 
 @RestController
 @RequestMapping(value = "/tutor")
@@ -30,8 +24,7 @@ public class TutorController {
   TutorService tutorService;
 
   @PostMapping("/register")
-  public ResponseEntity<String> register( String name ,String surname , Date birth) {
-        Tutor tutor = new Tutor(0, name, surname, birth, null);
+  public ResponseEntity<String> register(Tutor tutor) {
     if (tutorService.register(tutor)) {
       return ResponseEntity.ok("Tutor successfully saved!");
     }
@@ -45,12 +38,12 @@ public class TutorController {
 
   @DeleteMapping(value = "/deleteById/{id}")
   public ResponseEntity<String> deleteById(@PathVariable("id") int id) {
-    if (tutorService.deleteById(id)) 
-    { return ResponseEntity.ok("Tutor successfully deleted");
-    
+    if (tutorService.deleteById(id)) {
+      return ResponseEntity.ok("Tutor successfully deleted");
+
     }
-     return ResponseEntity.ok("Tutor not found");
-    
+    return ResponseEntity.ok("Tutor not found");
+
   }
 
   @GetMapping(value = "/list")
@@ -62,8 +55,8 @@ public class TutorController {
   public ResponseEntity<String> updateTutorByName(@PathVariable String name, @RequestBody Tutor updatedTutor) {
     if (tutorService.updateTutorByName(name, updatedTutor)) {
       return ResponseEntity.ok("Tutor updated successfully.");
-    } 
-      return ResponseEntity.notFound().build();
+    }
+    return ResponseEntity.notFound().build();
   }
 
   @GetMapping(value = "/searchByName/{name}")
